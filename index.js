cddd2d58ab5c9a047218e1b4f1d9593055a6db58
mzteepowell <mzteepowell@gmail.com>
2021-01-29 13:17:22 -0500

    Topic 3: Step 2 Complete

--- index.js
+++ index.js
@@ -163,9 +163,9 @@ function CuboidMaker(attributes) {
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 
-
-
-
+  CuboidMaker.prototype.volume = function() {
+    return this.length * this.width * this.height;
+  }
 
 /* 🐴🐴🐴 Step 3: Surface Area Method 🐴🐴🐴
   Create another method called surfaceArea using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
