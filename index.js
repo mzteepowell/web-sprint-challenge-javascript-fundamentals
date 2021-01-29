06075e3466f4834f73e86a74440a9695e436133d
mzteepowell <mzteepowell@gmail.com>
2021-01-29 12:29:07 -0500

    Task 5 Complete

--- index.js
+++ index.js
@@ -112,9 +112,9 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
-  }
+ function consume(a, b, cb) {
+  return cb(a,b);
+}
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
