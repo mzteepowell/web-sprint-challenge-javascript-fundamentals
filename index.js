edc2062ca098af7f130e6b9b65c820ee8d296181
mzteepowell <mzteepowell@gmail.com>
2021-01-29 11:21:35 -0500

    Task 1 Complete

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+/* Line 11 is a nested function because it is a function created within a function.
 
 
 
@@ -28,9 +28,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  let total = 0;
+  for (let i = 0; i < number; i++) {
+    total = total + (number - i); 
+  }
+  return total;
   }
  
 
